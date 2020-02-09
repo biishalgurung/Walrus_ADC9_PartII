824f2659b3d6fc3272baacbd489122f7fa54eccb
DupTseringLepcha <58845486+DupTseringLepcha@users.noreply.github.com>
2020-02-09 19:25:49 +0545

    Test And Logout

--- estate/models.py
+++ estate/models.py
@@ -8,11 +8,21 @@ class Property(models.Model):
     property_detail=models.TextField()
     uploaded_at=models.DateTimeField(auto_now_add=True)
 
+    def valid_user(self):
+        return self.property_name!=""
+    
+    def valid_price(self):
+        return self.price>0
+
+
 class Customer(models.Model):
     customer_name=models.TextField()
     customer_address=models.TextField()
     customer_phone=models.IntegerField()
     customer_details=models.TextField()
+
+    def valid_phone(self):
+        return self.customer_phone>0
     
 
 class Admin(models.Model):
@@ -21,9 +31,15 @@ class Admin(models.Model):
     admin_phone=models.IntegerField()
     admin_address=models.TextField()
 
+    def valid_address(self):
+        return self.admin_address!=""
+
 class Owner(models.Model):
     owner_name=models.TextField()
     owner_address=models.TextField()
     owner_details=models.TextField()
     owner_phone=models.IntegerField()
     uploaded_at=models.DateTimeField(auto_now_add=True)
+
+    def valid_phone(self):
+        return self.owner_phone>0
--- estate/tests.py
+++ estate/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from estate.models import *
+
+class ModelTestCase(TestCase):
+
+    def test_valid_user(self):
+        property=Property.objects.create(property_name="abc",price=1500,property_detail="This is very good")
+
+        self.assertTrue(property.valid_user())
+    
+    def test_customer_valid_phone(self):
+        customer=Customer.objects.create(customer_name="Dup Raja",customer_address="Ktm City",customer_phone=987654321,customer_details="Very Good Fellow")
+        self.assertTrue(customer.valid_phone())
+    
+    def test_valid_address(self):
+        admin=Admin.objects.create(admin_name="Dup Raja",admin_address="Ktm City",admin_phone=987654321,admin_details="Very Good Fellow")
+        self.assertTrue(admin.valid_address())
+    
+    def test_owner_valid_phone(self):
+        owner=Owner.objects.create(owner_name="Dup Raja",owner_address="Ktm City",owner_phone=987654321,owner_details="Very Good Fellow")
+        self.assertTrue(owner.valid_phone())
